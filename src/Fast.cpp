65b18ff40299eacdf671967f300cf77f0f8fa356
JohnMave126 <johnmave126@gmail.com>
2013-04-22 23:15:39 +0800

    Add a naive Fast test program.
    Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- src/Fast.cpp
+++ src/Fast.cpp
@@ -16,3 +16,26 @@ bool isCollinear(const Point &p1, const Point &p2) const {
     //Collinear if sin<p1-this, p2-this> = 0
     return ((p1 - (*this)).cross(p2 - (*this))) == 0;
 }
+
+int main(int argc, char **argv) {
+    int i;
+    Deque<LineSeg> res;
+    Deque<LineSeg>::Iterator itr(res);
+    Point *p_arr;
+    int size;
+    ifstream is("input.txt", ifstream::in);
+    is >> size;
+    p_arr = new Point[size];
+
+    for(i = 0; i < size; i++) {
+        is >> p_arr[i];
+    }
+
+    res = Fast(p_arr, size);
+
+    for(i = 0; i < res.size(); i++, itr++) {
+        cout << (*itr) << endl;
+    }
+    delete [] p_arr;
+    return 0;
+}
