b40f8f2978de3087206e883fef3ad52c112eb3d6
JohnMave126 <johnmave126@gmail.com>
2013-04-26 01:26:52 +0800

    Add rich command line support. Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- src/Brute.cpp
+++ src/Brute.cpp
@@ -18,15 +18,79 @@
 #include <fstream>
 
 using std::ifstream;
+using std::cin;
 using std::cout;
 using std::endl;
+
+void print_help() {
+    cout << "Usage: Brute [OPTION]..." << endl;
+    cout << "Process a series of points and find lines." << endl;
+    cout << endl;
+    cout << "  -i FILE               use FILE as input instead of stdin" << endl;
+    cout << "  -h                    display this help and exit" << endl;
+    cout << endl;
+    cout << "Brute takes input from standard input or file, and find" << endl;
+    cout << "lines on which there are four points using Brute Force" << endl;
+    cout << "algorithm." << endl;
+    cout << endl;
+    cout << "Written by TAN, Shuhao <stanab@stu.ust.hk>" << endl;
+}
 
 int main(int argc, char **argv) {
     Deque<LineSeg> res;
     Point *p_arr;
     int size;
-    ifstream is("input.txt", ifstream::in);
+    //stdin
+    bool flag = true;
+    istream is;
     
+    //Process command-line input
+    //Omit the filename
+    i = 1;
+    while(i < argc) {
+        if(argv[i][0] == '-') {
+            if(strlen(argv[i]) != 2) {
+                cout << "Brute: unknown option: " << argv[i] << endl;
+                cout << "Try `Brute -h' for more information." << endl;
+                exit(EXIT_FAILURE);
+            }
+            switch(argv[i][1]) {
+                case 'h':
+                    //Help
+                    print_help();
+                    exit(EXIT_SUCCESS);
+                case 'i':
+                    //Another input file
+                    if(!flag) {
+                        cout << "Brute: duplicated option: " << argv[i] << endl;
+                        cout << "Try `Brute -h' for more information." << endl;
+                        exit(EXIT_FAILURE);
+                    }
+                    i++;
+                    is = ifstream(argv[i], ifstream::in);
+                    if(!is) {
+                        cout << "Brute: file not found: " << argv[i] << endl;
+                        cout << "Try `Brute -h' for more information." << endl;
+                        exit(EXIT_FAILURE);
+                    }
+                    flag = false;
+                default:
+                    //Unrecognized
+                    cout << "Brute: unknown option: " << argv[i] << endl;
+                    cout << "Try `Brute -h' for more information." << endl;
+                    exit(EXIT_FAILURE);
+            }
+        }
+        else {
+            cout << "Brute: invalid input" << endl;
+            cout << "Try `Brute -h' for more information." << endl;
+        }
+        i++;
+    }
+
+    if(flag) {
+        is = cin;
+    }
     p_arr = readPoints(is, &size);
     res = Brute(p_arr, size);
     printLines(cout, res);
--- src/Fast.cpp
+++ src/Fast.cpp
@@ -18,15 +18,79 @@
 #include <fstream>
 
 using std::ifstream;
+using std::cin;
 using std::cout;
 using std::endl;
+
+void print_help() {
+    cout << "Usage: Fast [OPTION]..." << endl;
+    cout << "Process a series of points and find lines." << endl;
+    cout << endl;
+    cout << "  -i FILE               use FILE as input instead of stdin" << endl;
+    cout << "  -h                    display this help and exit" << endl;
+    cout << endl;
+    cout << "Fast takes input from standard input or file, and find" << endl;
+    cout << "lines on which there are four points using Fast" << endl;
+    cout << "algorithm." << endl;
+    cout << endl;
+    cout << "Written by TAN, Shuhao <stanab@stu.ust.hk>" << endl;
+}
 
 int main(int argc, char **argv) {
     Deque<LineSeg> res;
     Point *p_arr;
     int size;
-    ifstream is("input.txt", ifstream::in);
+    //stdin
+    bool flag = true;
+    istream is;
+    
+    //Process command-line input
+    //Omit the filename
+    i = 1;
+    while(i < argc) {
+        if(argv[i][0] == '-') {
+            if(strlen(argv[i]) != 2) {
+                cout << "Fast: unknown option: " << argv[i] << endl;
+                cout << "Try `Fast -h' for more information." << endl;
+                exit(EXIT_FAILURE);
+            }
+            switch(argv[i][1]) {
+                case 'h':
+                    //Help
+                    print_help();
+                    exit(EXIT_SUCCESS);
+                case 'i':
+                    //Another input file
+                    if(!flag) {
+                        cout << "Fast: duplicated option: " << argv[i] << endl;
+                        cout << "Try `Fast -h' for more information." << endl;
+                        exit(EXIT_FAILURE);
+                    }
+                    i++;
+                    is = ifstream(argv[i], ifstream::in);
+                    if(!is) {
+                        cout << "Fast: file not found: " << argv[i] << endl;
+                        cout << "Try `Fast -h' for more information." << endl;
+                        exit(EXIT_FAILURE);
+                    }
+                    flag = false;
+                default:
+                    //Unrecognized
+                    cout << "Fast: unknown option: " << argv[i] << endl;
+                    cout << "Try `Fast -h' for more information." << endl;
+                    exit(EXIT_FAILURE);
+            }
+        }
+        else {
+            cout << "Fast: invalid input" << endl;
+            cout << "Try `Fast -h' for more information." << endl;
+        }
+        i++;
+    }
 
+    if(flag) {
+        is = cin;
+    }
     p_arr = readPoints(is, &size);
     res = Fast(p_arr, size);
     printLines(cout, res);
