35e6c126cd5a69ba6eb23ed0a40a73f268bb2486
JohnMave126 <johnmave126@gmail.com>
2013-04-25 14:00:34 +0800

    Add GUI files. Signed-off-by: JohnMave126 <johnmave126@gmail.com>

---
+++ include/patterns_window.h
@@ -0,0 +1,97 @@
+/*
+ * Patterns for COMP2012H
+ * Author: TAN, Shuhao
+ * Stu ID: 20090398
+ * 2013 Spring
+ *
+ * patterns_window.h
+ *
+ * header file for graphical interface
+ */
+
+#ifndef _PATTERNS_WINDOW_H
+#define _PATTERNS_WINDOW_H
+
+#include "patterns_algorithm.h"
+#include "patterns_common.h"
+#include "Point.h"
+#include "deque.h"
+#include <qmainwindow.h>
+#include <qapplication.h>
+
+class PatternsMainWindow: public QMainWindow {
+    // All classes that contain signals or slots
+    // must mention Q_OBJECT in their declaration.
+    Q_OBJECT
+
+    public:
+        PatternsMainWindow(QWidget* parent = 0, const char* name = 0);
+        ~PatternsMainWindow();
+
+    public slots:
+        //File Menu begins
+
+        /*
+         * OnLoadFile
+         *
+         * slot that handle action when try to load from a file
+         */
+        void OnLoadFile();
+
+        /*
+         * onLoadBrute
+         *
+         * slot that handle action when try to run Brute Force algorithm
+         */
+        void onLoadBrute();
+
+        /*
+         * onLoadFast
+         *
+         * slot that handle action when try to run Fast algorithm
+         */
+        void onLoadFast();
+
+        /*
+         * onExit
+         *
+         * slot that handle action when try to Exit
+         */
+        void onExit();
+        //File Menu End
+
+        //Help Menu Begin
+        
+        /*
+         * ShowAbout
+         *
+         * slot that handle action when try to show About info
+         */
+        void ShowAbout();
+        //Help Menu End
+        
+    protected:
+        /** handler for paint event */
+        virtual void paintEvent(QPaintEvent*);
+
+    private:
+        
+        /*
+         * processPoints
+         *
+         * process input points for convenience
+         */
+        void processPoints();
+
+        //Store Points
+        Point *p_arr;
+        int size;
+
+        //Store result
+        Deque<LineSeg> result;
+
+        //Painting related
+        int min_x, max_x, min_y, max_y;
+};
+
+#endif
---
+++ src/patterns_window.cpp
@@ -0,0 +1,162 @@
+/*
+ * Patterns for COMP2012H
+ * Author: TAN, Shuhao
+ * Stu ID: 20090398
+ * 2013 Spring
+ *
+ * patterns_window.cpp
+ *
+ * graphical interface for Patterns
+ */
+
+#include "patterns_window.h"
+#include "patterns_common.h"
+#include <ctime>
+#include <iostream>
+#include <qmainwindow.h>
+#include <qapplication.h>
+#include <qmessagebox.h>
+
+using std::cout;
+
+PatternsMainWindow::PatternsMainWindow(QWidget* parent, const char* name) 
+:QMainWindow(parent, name), p_arr(NULL), size(0) {
+    //Create File Menu
+    //File Menu Begin
+    QPopupMenu* file = new QPopupMenu(this);
+
+    //Create Load and Calculate Section
+    file->insertItem("&Load input file", this, SLOT(OnLoadFile()), CTRL+Key_N);
+    file->insertItem("Load &Brute Algorithm", this, SLOT(onLoadBrute()), CTRL+Key_B);
+    file->insertItem("Load &Fast Algorithm", this, SLOT(onLoadFast()), CTRL+Key_F);
+    file->insertSeparator();
+    
+    //Create Exit Section
+    file->insertItem("&Exit", this, SLOT(OnExit()), CTRL+Key_X);
+
+    //Insert File Menu
+    menuBar()->insertItem("&File", file);
+    //File Menu End
+
+    //Create Help Menu
+    //Help Menu Begin
+    QPopupMenu* help = new QPopupMenu(this);
+    
+    //Create Help items
+    help->insertItem("&About", this, SLOT(ShowAbout()));
+    
+    //Insert Help Menu
+    menuBar()->insertItem("&Help", help);
+    //Help Menu End
+}
+
+PatternsMainWindow::~PatternsMainWindow() {
+    if(p_arr) {
+        delete [] p_arr;
+    }
+}
+
+void PaintMainWindow::OnLoadFile() {
+    //Prompt a window to get filename
+    QString filename = QFileDialog::getOpenFileName(QString::null,
+        "All Files (*.*)", this, 0, "Load input file...");
+    
+    //Something meaningful be input
+    if(!filename.isNull()) {
+        QFile file(filename);
+        if(file.open(IO_ReadOnly)) {
+            QTextStream is(&file);
+            if(p_arr) {
+                delete [] p_arr;
+            }
+            p_arr = readPoints(is, &size);
+            //Pre process
+            processPoints();
+            result = Deque<LineSeg>();
+
+            //Call for repaint
+            repaint();
+        }
+    }
+}
+
+void PaintMainWindow::onLoadBrute() {
+    clock_t last_tick, cur_tick;
+    if(!p_arr) {
+        QMessageBox::critical( this, "No File loaded",
+            "No file has been loaded!\n"
+            "Please use File->Load input file to load one.",
+            QMessageBox::Ok, QMessageBox::NoButton, QMessageBox::NoButton);
+    }
+    else {
+        last_tick = clock();
+        result = Brute(p_arr, size);
+        cur_tick = clock();
+
+        printLines(cout, result);
+        QMessageBox::information( this, "Running time",
+            QString("%1 ms").arg(
+                1000.0 * (cur_tick - last_tick) / CLOCKS_PER_SEC, 'f', 2) );
+
+        //Call for repaint
+        repaint();
+    }
+}
+
+void PaintMainWindow::onLoadFast() {
+    clock_t last_tick, cur_tick;
+    if(!p_arr) {
+        QMessageBox::critical( this, "No File loaded",
+            "No file has been loaded!\n"
+            "Please use File->Load input file to load one.",
+            QMessageBox::Ok, QMessageBox::NoButton, QMessageBox::NoButton);
+    }
+    else {
+        last_tick = clock();
+        result = Fast(p_arr, size);
+        cur_tick = clock();
+
+        printLines(cout, result);
+        QMessageBox::information( this, "Running time",
+            QString("%1 ms").arg(
+                1000.0 * (cur_tick - last_tick) / CLOCKS_PER_SEC, 'f', 2) );
+
+        //Call for repaint
+        repaint();
+    }
+}
+
+void PatternsMainWindow::onExit() {
+    qApp->quit();
+}
+
+void PatternsMainWindow::ShowAbout() {
+    QMessageBox::information( this, "About",
+        "COMP2012H Project4 Patterns\n"
+        "By TAN, Shuhao(20090398)");
+}
+
+void PatternsMainWindow::paintEvent(QPaintEvent*) {
+
+}
+
+void PatternsMainWindow::processPoints() {
+    int i;
+    min_x = min_y = 0;
+    max_x = max_y = 32768;
+    for(i = 0; i < size; i++) {
+        //Process the region of display
+        if(p_arr[i].getX() < min_x) {
+            min_x = p_arr[i].getX();
+        }
+        else if(p_arr[i].getX() > max_x) {
+            max_x = p_arr[i].getX();
+        }
+        if(p_arr[i].getY() < min_y) {
+            min_y = p_arr[i].getY();
+        }
+        else if(p_arr[i].getY() > max_y) {
+            max_y = p_arr[i].getY();
+        }
+    }
+}
